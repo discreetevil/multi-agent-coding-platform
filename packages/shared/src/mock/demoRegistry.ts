97ac0bdc519e45cb323ba8d66d3b212eae3bb9ad
discreetevil <hassanseif004@gmail.com>
2026-08-12 00:08:10 +0300

    fix: correct demo registry import paths

--- packages/shared/src/mock/demoRegistry.ts
+++ packages/shared/src/mock/demoRegistry.ts
@@ -1,8 +1,6 @@
-import { Task } from './interfaces/Task';
-import { AgentRegistry } from './registry/AgentRegistry';
-import { SimpleAgentRouter } from './router/SimpleAgentRouter';
-import { MockAgentAdapter } from './mock/MockAgentAdapter';
-import { AgentDescriptor } from './interfaces/AgentDescriptor';
+import { AgentRegistry } from '../registry/AgentRegistry';
+import { MockAgentAdapter } from './MockAgentAdapter';
+import { AgentDescriptor } from '../interfaces/AgentDescriptor';
 
 export async function createDemoRegistry(): Promise<AgentRegistry> {
   const reg = new AgentRegistry();
